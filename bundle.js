62971e23e3586d8412aafca43015110629b690c9
victor bello <bellomanuel1502@gmail.com>
2025-09-21 13:10:31 -0600

    Primer commit del proyecto

---
+++ bundle.js
@@ -0,0 +1,20 @@
+function App() {
+  const div = document.createElement('div');
+  div.innerHTML += `<p>Este es el footer</p>`;
+  return div;
+}
+
+function xd() {
+  const div = document.createElement('div');
+  div.innerHTML += `<h1>Bienvenido a Moon 🚀</h1>`;
+  div.innerHTML += `<h2>Hola!</h2>`;
+  div.innerHTML += `<p style ="color:red;">Este es el header</p>`;
+  return div;
+}
+
+function Footer() {
+  const div = document.createElement('div');
+  div.innerHTML += `<footer>Footer</footer>`;
+  return div;
+}
+
---
+++ components/xd.moon
@@ -0,0 +1,3 @@
+<h1>Bienvenido a Moon 🚀</h1>
+<h2>Hola!</h2>
+<p style ="color:red;">Este es el header</p>
